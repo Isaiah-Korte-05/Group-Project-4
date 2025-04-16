970d0a11ef856eaa52339382054482eb58cf1fcd
anna <150392971+annamori900@users.noreply.github.com>
2025-04-16 09:24:40 -0500

    added Player class members and basic methods

--- TearsDespairAndDebugging/src/hw4/player/Player.java
+++ TearsDespairAndDebugging/src/hw4/player/Player.java
@@ -1,5 +1,36 @@
 package hw4.player;
 
+import hw4.maze.*;
+
 public class Player {
+	
+	private Row currentRow;
+	private Cell currentCell;
+	
+	public Player(Row row, Cell cell) {
+		setCurrentRow(row);
+		setCurrentCell(cell);
+	}
+
+	public Row getCurrentRow() {
+		return currentRow;
+	}
+
+	public void setCurrentRow(Row currentRow) {
+		this.currentRow = currentRow;
+	}
+
+	public Cell getCurrentCell() {
+		return currentCell;
+	}
+
+	public void setCurrentCell(Cell currentCell) {
+		this.currentCell = currentCell;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [currentRow=" + currentRow + ", currentCell=" + currentCell + "]";
+	}
 
 }
