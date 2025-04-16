024b4e0771ccd4ae9e92d90dfc927a21b9b70888
anna <150392971+annamori900@users.noreply.github.com>
2025-04-16 09:52:45 -0500

    updated createRandomGrid(int) method in Game class

--- TearsDespairAndDebugging/src/hw4/game/Game.java
+++ TearsDespairAndDebugging/src/hw4/game/Game.java
@@ -1,5 +1,8 @@
 package hw4.game;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import hw4.maze.*;
 import hw4.player.*;
 
@@ -35,7 +38,47 @@ public class Game {
 	
 	public Grid createRandomGrid(int size) {
 		if (size < 3 || size > 7) { return null; }
-		return new Grid();
+		
+		Random rand = new Random();
+		ArrayList<Row> rows = new ArrayList<>();
+		int exitRowIndex = rand.nextInt(size);
+		for (int rowIndex = 0; rowIndex < size; rowIndex++) {
+			ArrayList<Cell> cells = new ArrayList<>();
+			for (int cellIndex = 0; cellIndex < size; cellIndex++) {
+				CellComponents left;
+				CellComponents right;
+				CellComponents up;
+				CellComponents down;
+				if (cellIndex == 0) {
+					left = rowIndex == exitRowIndex ? CellComponents.EXIT : getRandomCellComponent();
+				}
+				else {
+					left = cells.get(cellIndex - 1).getRight();
+				}
+				if (rowIndex == 0) {
+					up = getRandomCellComponent();
+				}
+				else {
+					up = rows.get(rowIndex - 1).getCells().get(cellIndex).getDown();
+				}
+				right = getRandomCellComponent();
+				down = getRandomCellComponent();
+				Cell cell = new Cell(left, right, up, down);
+				while (!(cell.hasAperture() && cell.hasWall())) {
+					right = getRandomCellComponent();
+					down = getRandomCellComponent();
+					cell = new Cell(left, right, up, down);
+				}
+				cells.add(cell);
+			}
+			rows.add(new Row(cells));
+		}
+		return new Grid(rows);
+	}
+	
+	private CellComponents getRandomCellComponent() {
+		Random rand = new Random();
+		return rand.nextBoolean() ? CellComponents.WALL : CellComponents.APERTURE;
 	}
 
 }
