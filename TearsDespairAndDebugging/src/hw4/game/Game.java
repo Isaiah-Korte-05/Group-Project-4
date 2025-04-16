a261377c4ce10a265075ef836e436b0b4b0ac7c7
anna <150392971+annamori900@users.noreply.github.com>
2025-04-16 18:08:06 -0500

    added javadoc and comments in play() and createRandomGrid()

--- TearsDespairAndDebugging/src/hw4/game/Game.java
+++ TearsDespairAndDebugging/src/hw4/game/Game.java
@@ -10,57 +10,88 @@ public class Game {
 	
 	private Grid grid;
 	
+	/**
+	 * Parameterized constructor to correspond a grid with this game
+	 * @param grid A Grid in which the game uses
+	 */
 	public Game(Grid grid) {
 		setGrid(grid);
 	}
 	
+	/**
+	 * Parameterized constructor to correspond a random grid of the given size with this game
+	 * @param gridSize An int detailing the desired size of the game's grid
+	 */
 	public Game(int gridSize) {
 		setGrid(createRandomGrid(gridSize));
 	}
 
+	/**
+	 * Getter for the class member holding the game's Grid
+	 * @return The Grid of the game
+	 */
 	public Grid getGrid() {
 		return grid;
 	}
 
+	/**
+	 * Setter for the class member holding the game's Grid
+	 * @param grid The Grid of the game
+	 */
 	public void setGrid(Grid grid) {
 		this.grid = grid;
 	}
 
+	/**
+	 * Formats the Game class and its class member into a string
+	 * @return The String formatting the class name and class member
+	 */
 	@Override
 	public String toString() {
 		return "Game [grid=" + grid + "]";
 	}
 	
+	/**
+	 * Allows the player to move in the grid and play the game.
+	 * @param movement A Movement enum detailing the desired direction of travel of the player
+	 * @param player The Player that wants to move in the grid
+	 * @return True if the Player and Movement choice are valid and the player has not yet exited, false otherwise
+	 */
 	public boolean play(Movement movement, Player player) {
 		if (movement == null || player == null) { return false; }
-		
 		if (player.hasExited()) { return false; }
 		
+		// Get player's current position in the grid
 		ArrayList<Row> row = grid.getRows();
 		int rowIndex = row.indexOf(player.getCurrentRow());
 		int cellIndex = player.getCurrentRow().getCells().indexOf(player.getCurrentCell());
 		if (rowIndex == -1 || cellIndex == -1) { return false; }
-		
 		Cell currentCell = player.getCurrentCell();
 		
+		// Decide if passed Movement is valid
+		// If so, set player's position to their desired cell
 		switch(movement) {
 		case LEFT:
+			// Player is in cell with exit on left
 			if (currentCell.getLeft() == CellComponents.EXIT) {
 				player.setHasExited(true);
 				return true;
 			}
+			// Player has space to move left and is not on grid's left edge
 			if (currentCell.getLeft() == CellComponents.APERTURE && cellIndex > 0) {
 				player.setCurrentCell(row.get(rowIndex).getCells().get(cellIndex - 1));
 				return true;
 			}
 			break;
 		case RIGHT:
+			// Player has space to move right and is not on grid's right edge
 			if (currentCell.getRight() == CellComponents.APERTURE && cellIndex < row.get(rowIndex).getCells().size() - 1) {
 				player.setCurrentCell(row.get(rowIndex).getCells().get(cellIndex + 1));
 				return true;
 			}
 			break;
 		case UP:
+			// Player has space to move up and is not on grid's top edge
 			if (currentCell.getUp() == CellComponents.APERTURE && rowIndex > 0) {
 				Row nextRow = row.get(rowIndex - 1);
 				player.setCurrentRow(nextRow);
@@ -69,6 +100,7 @@ public class Game {
 			}
 			break;
 		case DOWN:
+			// Player has space to move down and is not on grid's bottom edge
 			if (currentCell.getDown() == CellComponents.APERTURE && rowIndex < row.size() - 1) {
 				Row nextRow = row.get(rowIndex + 1);
 				player.setCurrentRow(nextRow);
@@ -80,12 +112,20 @@ public class Game {
 		return false;
 	}
 	
+	/**
+	 * Creates a Grid of a given size with randomly decided CellComponents for each cell
+	 * @param size An int detailing the desired size (one side) of the square grid
+	 * @return A Grid of the desired size with random maze setup
+	 */
 	public Grid createRandomGrid(int size) {
 		if (size < 3 || size > 7) { return null; }
 		
+		// Randomly decide the row in which the exit sits
 		Random rand = new Random();
 		ArrayList<Row> rows = new ArrayList<>();
 		int exitRowIndex = rand.nextInt(size);
+		
+		// Generate Cell and Row ArrayLists with random CellComponents enums
 		for (int rowIndex = 0; rowIndex < size; rowIndex++) {
 			ArrayList<Cell> cells = new ArrayList<>();
 			for (int cellIndex = 0; cellIndex < size; cellIndex++) {
@@ -93,21 +133,31 @@ public class Game {
 				CellComponents right;
 				CellComponents up;
 				CellComponents down;
+				
+				// Sets left component to exit if cell is in the correct row, or random CellComponents otherwise
 				if (cellIndex == 0) {
 					left = rowIndex == exitRowIndex ? CellComponents.EXIT : getRandomCellComponent();
 				}
+				// Sets left component to random CellComponents
 				else {
 					left = cells.get(cellIndex - 1).getRight();
 				}
+				// Sets 
 				if (rowIndex == 0) {
 					up = getRandomCellComponent();
 				}
+				// Sets
 				else {
 					up = rows.get(rowIndex - 1).getCells().get(cellIndex).getDown();
 				}
+				// Sets right and down components to random CellComponents
 				right = getRandomCellComponent();
 				down = getRandomCellComponent();
+				
+				// Creates new Cell
 				Cell cell = new Cell(left, right, up, down);
+				
+				// While the Cell does not have at least one aperture and one wall, re-randomly set CellComponents
 				while (!(cell.hasAperture() && cell.hasWall())) {
 					right = getRandomCellComponent();
 					down = getRandomCellComponent();
@@ -120,6 +170,10 @@ public class Game {
 		return new Grid(rows);
 	}
 	
+	/**
+	 * Helper function that decides the CellComponent enum using the Random class
+	 * @return A randomly decided CellComponent enum
+	 */
 	private CellComponents getRandomCellComponent() {
 		Random rand = new Random();
 		return rand.nextBoolean() ? CellComponents.WALL : CellComponents.APERTURE;
