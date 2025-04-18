432dc18975a4ec909b579ec40c662053c75083c7
Isaiah-Korte-05 <ijkmmf@gmail.com>
2025-04-18 14:37:48 -0500

    updated comments

--- TearsDespairAndDebugging/src/hw4/maze/Grid.java
+++ TearsDespairAndDebugging/src/hw4/maze/Grid.java
@@ -39,12 +39,23 @@ public class Grid {
 		return "Grid [rows=" + rows + "]";
 	}
 	
+	/**
+	 * Converts cell state into graphical representation based on if it
+	 * is the exit, a regular space, or contains the player agent
+	 */
 	public void printGrid() {
+		
 		System.out.println("---------------\n CURRENT GRID\n---------------");
+		
+		// Iterates through rows
 		for(int i = 0; i < rows.size(); i++) {
+			
+			// Iterates through each cell in a row
 			for(int j = 0; j < rows.get(i).getCells().size(); j++) {
+				
+				// Determines correct graphical representation based on cell state/data
 				if(rows.get(i).getCells().get(j).getHasPlayer()) {
-					System.out.print("[A]");
+					System.out.print(" A ");
 				}
 				else if(rows.get(i).getCells().get(j).getLeft() == CellComponents.EXIT) {
 					System.out.print(" E ");
